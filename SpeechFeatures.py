cde9e4daac7b5cc840a51c341285d59b59bd39c5
Claudia <ckanter@kth.se>
2017-10-15 17:50:55 +0200

    added basic feature extraction

---
+++ SpeechFeatures.py
@@ -0,0 +1,70 @@
+import sidekit.frontend.features as ff
+from scikits.audiolab import wavread
+import numpy as np
+import pdb
+
+class SpeechFeatures():
+	def getFeatures(self, signal, fs, winlength, *args):
+		if len(args) == 0:
+			calcmfccs = False
+			ncep = 1
+		elif len(args) == 1:
+			calcmfccs = True
+			ncep = args[0]
+		else:
+			print 'Incorrect number of inputs.'
+
+		signal = np.real(signal)
+
+		if fs <= 0:
+			fs = 44100
+
+		if isinstance(ncep, list):
+			ncep = round(np.real(ncep[0]))
+		else:
+			ncep = round(np.real(ncep))
+
+		if ncep < 1:
+			ncep = 1
+
+		if isinstance(winlength, list):
+			winlength = np.real(winlength[0])
+		else:
+			winlength = np.real(winlength)
+
+		if winlength * fs < ncep:
+			winlength = ncep / fs
+
+		winshift = 0.5
+		minfreq = 20
+		maxfreq = 4000
+		nbands = 30
+		lifterexp = 0
+		preemph = 0
+
+		if calcmfccs == True:
+			ceps, log_energy, spec, mspec = ff.mfcc(signal, lowfreq=minfreq, maxfreq=maxfreq, nlinfilt=lifterexp, 
+				nlogfilt=nbands, nwin=winlength, fs=fs, nceps=int(ncep), shift=winlength*winshift, prefac=preemph)
+
+			output = []
+			output.append(ceps)
+		else:
+			output = []
+
+		if not output:
+			spectram, log_energy = ff.power_spectrum(signal, fs=fs, win_time=winlength, shift=winlength*winshift, prefac=1)
+			output.append(spectram)
+			output.append(log_energy)
+
+		return output
+
+	def dynamic_features(self, ceps):
+		delta = np.diff(ceps)
+		delta_delta = np.diff(ceps)
+
+	def wavread(self, filename):
+		s, fs, enc = wavread(filename)  # s in range -32768 to +32767
+		s = np.array(s)
+		s = s / max(abs(s)) 
+
+		return (s,fs,enc)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import pdb
+
 import numpy as np
+from SpeechFeatures import SpeechFeatures
 from MarkovChain import MarkovChain
 from GaussD import GaussD
 from HMM import HMM
+import matplotlib.pyplot as plt
 
 def finite_duration():
 	initMatrix = np.matrix([[0.75], [0.25]])
@@ -11,8 +15,6 @@ def finite_duration():
 	g2 = GaussD(mean=np.matrix([3]), stdev=np.matrix([2]))
 	h = HMM(markovChain, np.matrix([[g1], [g2]]))
 	[X,S] = h.rand(100)
-	print 'X =', X
-	print 'S =', S
 
 	return (X,S)
 
@@ -24,11 +26,42 @@ def multi_dim_observation():
 	g2 = GaussD(mean=np.matrix([[3], [3]]), cov=np.matrix([[2, 1], [1, 4]]))
 	h = HMM(markovChain, np.matrix([[g1], [g2]]))
 	[X,S] = h.rand(100)
-	print 'X =', X
-	print 'S =', S
 
 	return (X,S)
 
+def extract_features(path, title):
+	sf = SpeechFeatures()
+	signal, fs, enc = sf.wavread(path)
+	t = np.array(range(1,signal.shape[0])) / float(fs);
+	#plt.plot(signal)
+
+	winlength = 0.03 # 30ms
+	
+	## get spectram
+	output = sf.getFeatures(signal, fs, winlength)
+	spectram = output[0]
+	log_energy = output[1]
+	#plt.imshow(np.log(spectram.T), origin='lower', extent=[0, t[-1], 0, fs])
+	#plt.xlabel('time [s]')
+	#plt.ylabel('frequency [Hz]')
+	#plt.axis('auto')
+
+	## get cepstragram
+	ncep = 13
+	cepstragram = sf.getFeatures(signal, fs, winlength, ncep)
+	plt.figure()
+	plt.title(title)
+	plt.imshow(cepstragram[0].T, origin='lower')
+	plt.axis('auto')
+
+
 if __name__ == "__main__":
 	#(X,S) = finite_duration()
-	(X,S) = multi_dim_observation()
+	#(X,S) = multi_dim_observation()
+	
+	#print 'X =', X
+	#print 'S =', S
+
+	extract_features('/home/claudia/Documents/Studium/Master/3.Semester/PatternRecognition/Project/Sounds/female.wav', 'female')
+	extract_features('/home/claudia/Documents/Studium/Master/3.Semester/PatternRecognition/Project/Sounds/male.wav', 'male')
+	plt.show()
